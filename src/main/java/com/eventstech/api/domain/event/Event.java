af36a04d54eada079aee4a7bdcfce2eb011366e5
José Guillardi <63321040+JoseGu1llardi@users.noreply.github.com>
2024-06-30 21:15:25 +0100

    feat: added repositories

--- src/main/java/com/eventstech/api/domain/event/Event.java
+++ src/main/java/com/eventstech/api/domain/event/Event.java
@@ -1,9 +1,7 @@
 package com.eventstech.api.domain.event;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.eventstech.api.domain.address.Address;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,8 +24,11 @@ public class Event {
 
     private String title;
     private String description;
-    private String imgUrl;
+    private String imageUrl;
     private String eventUrl;
     private Boolean remote;
     private Date date;
+
+    @OneToOne(mappedBy = "event", cascade = CascadeType.ALL)
+    private Address address;
 }
